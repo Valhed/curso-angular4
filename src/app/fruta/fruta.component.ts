3c54dcc8922e37a8f9662c015012305cf9a690d5
Valhed <david.valderrama.h@gmail.com>
2018-04-01 21:02:16 -0300

    Commit tipos de variables

--- src/app/fruta/fruta.component.ts
+++ src/app/fruta/fruta.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component} from '@angular/core'; 
 
 @Component ({
     selector: 'fruta',
@@ -8,4 +8,11 @@ import {Component} from '@angular/core';
 export class FrutaComponent{
     public nombre_componente = 'Componente de fruta';
     public listado_fruta = 'Naranja,Mansana,Pera y sandia';
+
+    public nombre:String = "David Valderrama";
+    public edad:number = 26;
+    public mayorDeEdad:boolean=true;
+    public trabajos:Array<string> = ["Carpintero","Albaíl","Fontanero"];
+    public Array1:Array<any> = ["Carpintero",44,"Fontanero"]
+    public comodin = "cualquier cosa"
 }
