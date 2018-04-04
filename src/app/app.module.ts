38d73956912fc20de0a5fc43865f694fe7103d9c
Valhed <david.valderrama.h@gmail.com>
2018-04-03 23:26:55 -0300

    Commit Parte 4 completa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
@@ -14,7 +16,9 @@ import {EmpleadoComponent} from './empleado/empleado.component';
     EmpleadoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -11,6 +11,9 @@ export class EmpleadoComponent {
     public title = 'Componente Empleado';
     public empleado: Empleado;
     public trabajadores: Array<Empleado>;
+    public trabajador_externo: boolean;
+    public color: string;
+    public color_seleccionado: string;
 
     constructor() {
         this.empleado = new Empleado('David Valderrama', 26, 'cocinero', true);
@@ -19,10 +22,20 @@ export class EmpleadoComponent {
             new Empleado('David Lopez', 27, 'Chef', true),
             new Empleado('Gorge Valderrama', 36, 'ayudante de cocina', true)
         ];
+        this.trabajador_externo = false;
+        this.color = 'green';
+        this.color_seleccionado = '#ccc';
     }
     // tslint:disable-next-line:use-life-cycle-interface
     ngOnInit() {
         console.log(this.empleado);
         console.log(this.trabajadores);
+    }
+
+    cambiarExterno(valor) {
+        this.trabajador_externo = valor;
+    }
+    logColorSeleccionado() {
+        console.log(this.color_seleccionado);
     }
 }
