28f56477d85b7f94bd304abb49cfc6ba06bb2fa8
Valhed <david.valderrama.h@gmail.com>
2018-04-20 20:14:16 -0300

    06 Routing y navegación / 031 Menú de navegación

---
+++ src/app/app.routing.ts
@@ -0,0 +1,22 @@
+import {ModuleWithProviders} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+
+// Importar Componentes
+
+import {EmpleadoComponent} from './empleado/empleado.component';
+import {FrutaComponent} from './fruta/fruta.component';
+import {HomeComponent} from './home/home.component';
+import {ContactoComponent} from './contacto/contacto.component';
+
+const appRoutes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'empleado', component: EmpleadoComponent},
+    {path: 'fruta', component: FrutaComponent},
+    {path: 'contacto', component: ContactoComponent},
+    {path: 'home', component: HomeComponent},
+    {path: '**', component: HomeComponent}
+];
+
+export const appRoutingProviders: any[] = [];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
---
+++ src/app/contacto/contacto.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-home',
+    templateUrl: './contacto.component.html'
+})
+export class ContactoComponent {
+    public titulo = 'Página de contacto';
+}
